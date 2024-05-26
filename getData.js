53fff27ca11545fa3bbb839125a53d8c463746f9
Dionysus <124269615+WineOfDionysus@users.noreply.github.com>
2024-05-27 02:31:37 +0300

    added new plots(bad version)

--- getData.js
+++ getData.js
@@ -21,12 +21,18 @@ function setSettings()
 {
     var standart1 = "https://thingspeak.com/channels/2388828/charts/1?api_key=P270Q7US5PVK73F5&width=auto&height=auto&xaxis=Дата и время&title=Данные датчика температуры";
     var standart2 = "https://thingspeak.com/channels/2388828/charts/2?api_key=P270Q7US5PVK73F5&width=auto&height=auto&xaxis=Дата и время&title=Данные датчика влажности";
+    var standart3 = "https://thingspeak.com/channels/2388828/charts/1?api_key=P270Q7US5PVK73F5&width=auto&height=auto&xaxis=Дата и время&title=Данные датчика температуры";
+    var standart4 = "https://thingspeak.com/channels/2388828/charts/2?api_key=P270Q7US5PVK73F5&width=auto&height=auto&xaxis=Дата и время&title=Данные датчика влажности";
 
     var plot1 = document.getElementById("p1");
     var plot2 = document.getElementById("p2");
+    var plot3 = document.getElementById("p3");
+    var plot4 = document.getElementById("p4");
 
     plot1.src = standart1;
     plot2.src = standart2;
+    plot3.src = standart3;
+    plot4.src = standart4;
 
     if(fullScreen == 0)
     {
@@ -55,16 +61,22 @@ function setSettings()
     {
         plot1.src = plot1.src + "&type=line";
         plot2.src = plot2.src + "&type=line";
+        plot3.src = plot4.src + "&type=line";
+        plot4.src = plot3.src + "&type=line";
     }
     else if(plotsType.value == "spline")
     {
         plot1.src = plot1.src + "&type=spline";
         plot2.src = plot2.src + "&type=spline";
+        plot3.src = plot3.src + "&type=spline";
+        plot4.src = plot4.src + "&type=spline";
     }
     else if(plotsType.value == "column")
     {
         plot1.src = plot1.src + "&type=column";
         plot2.src = plot2.src + "&type=column";
+        plot3.src = plot3.src + "&type=column";
+        plot4.src = plot4.src + "&type=column";
     }
 
     var auto_update_checkbox = document.getElementById("auto-update");
@@ -72,12 +84,16 @@ function setSettings()
     {
         plot1.src = plot1.src + "&dynamic=true";
         plot2.src = plot2.src + "&dynamic=true";
+        plot3.src = plot3.src + "&dynamic=true";
+        plot4.src = plot4.src + "&dynamic=true";
     }
 
     if(document.getElementById("nums").value != "")
     {
         plot1.src = plot1.src + "&results=" + document.getElementById("nums").value;
         plot2.src = plot2.src + "&results=" + document.getElementById("nums").value;
+        plot3.src = plot3.src + "&results=" + document.getElementById("nums").value;
+        plot4.src = plot4.src + "&results=" + document.getElementById("nums").value;
     }
 
     var date_check = document.getElementById("datecheck");
@@ -94,6 +110,12 @@ function setSettings()
             "&end=" + dend.toISOString().substring(0,10) + "%20" + dend.toISOString().substring(11,19);
 
             plot2.src = plot2.src + "&start=" + dstart.toISOString().substring(0,10) + "%20" + dstart.toISOString().substring(11,19) +
+            "&end=" + dend.toISOString().substring(0,10) + "%20" + dend.toISOString().substring(11,19);
+
+            plot3.src = plot1.src + "&start=" + dstart.toISOString().substring(0,10) + "%20" + dstart.toISOString().substring(11,19) +
+            "&end=" + dend.toISOString().substring(0,10) + "%20" + dend.toISOString().substring(11,19);
+
+            plot4.src = plot2.src + "&start=" + dstart.toISOString().substring(0,10) + "%20" + dstart.toISOString().substring(11,19) +
             "&end=" + dend.toISOString().substring(0,10) + "%20" + dend.toISOString().substring(11,19);
         }
     }
